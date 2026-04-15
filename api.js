eb93ca8f321878f23b026673bf52b6a9da0f44cd
YLudvig <Ludvig.ysberg@gmail.com>
2026-04-15 13:21:47 +0200

    URL's in single spot for ease of changing for user (#82)

--- api.js
+++ api.js
@@ -1,12 +1,14 @@
+import {authURL} from "./apiURLs.js";
 // JS file to handle my API calls for the application
 
 // Handles the API part of the register user call,
 // the html connection is done in renderer.js
+
 export async function registerUser(name, password){
     const payload = {name, password};
 
     // Sending the data to backend mapping
-    const response = await fetch('http://localhost:8080/api/auth/register', {
+    const response = await fetch(`${authURL}/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify(payload)
@@ -28,7 +30,7 @@ export async function loginUser(name, password){
     const payload = {name, password};
 
     // Sending the data to backend mapping
-    const response = await fetch('http://localhost:8080/api/auth/login', {
+    const response = await fetch(`${authURL}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify(payload)
